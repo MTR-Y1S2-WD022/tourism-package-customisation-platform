46d0d24899fccf65276eeea9f6596c441be553d6
IT25103895 <it25103895@my.sliit.lk>
2026-05-01 14:00:39 +0530

    Initial commit

---
+++ src/main/java/com/tourismplatform/TourismplatformApplication.java
@@ -0,0 +1,13 @@
+package com.tourismplatform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourismplatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourismplatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tourismplatform/TourismplatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourismplatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismplatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
