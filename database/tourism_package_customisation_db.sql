717f20f9e3b9ba957863f82fee15f908649a3f7f
IT25103892 <IT25103892@my.sliit.lk>
2026-05-08 19:51:35 +0530

    Add PackageDestination model clas

---
+++ database/tourism_package_customisation_db.sql
@@ -0,0 +1,33 @@
+CREATE TABLE tour_packages (
+                               package_id INT PRIMARY KEY AUTO_INCREMENT,
+                               package_name VARCHAR(100) NOT NULL,
+                               location_area VARCHAR(100) NOT NULL,
+                               description TEXT,
+                               base_price DECIMAL(10,2) NOT NULL,
+                               image_url VARCHAR(255),
+                               status VARCHAR(20) DEFAULT 'ACTIVE',
+                               created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE destinations (
+                              destination_id INT PRIMARY KEY AUTO_INCREMENT,
+                              destination_name VARCHAR(100) NOT NULL,
+                              description TEXT,
+                              image_url VARCHAR(255),
+                              google_map_url VARCHAR(500),
+                              base_cost DECIMAL(10,2) DEFAULT 0.00,
+                              status VARCHAR(20) DEFAULT 'ACTIVE',
+                              created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE package_destinations (
+                                      id INT PRIMARY KEY AUTO_INCREMENT,
+                                      package_id INT NOT NULL,
+                                      destination_id INT NOT NULL,
+
+                                      FOREIGN KEY (package_id) REFERENCES tour_packages(package_id)
+                                          ON DELETE CASCADE,
+
+                                      FOREIGN KEY (destination_id) REFERENCES destinations(destination_id)
+                                          ON DELETE CASCADE
+);
