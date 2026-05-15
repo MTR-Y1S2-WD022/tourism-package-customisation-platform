86ed5c8f2254c8096b2ff947c941d8d7e01f7b70
IT25103894 <it25103894@my.sliit.lk>
2026-05-15 09:41:16 +0530

    Add BookingCancellationRequest model class

---
+++ src/main/java/com/tourismplatform/model/BookingCancellationRequest.java
@@ -0,0 +1,112 @@
+package com.tourismplatform.model;
+
+import java.time.LocalDateTime;
+
+public class BookingCancellationRequest {
+
+    private int requestId;
+    private int bookingId;
+    private int userId;
+
+    private String reason;
+    private String requestStatus;
+
+    private Integer adminId;
+    private String adminResponse;
+
+    private String refundType;
+    private double refundAmount;
+
+    private LocalDateTime requestedAt;
+    private LocalDateTime processedAt;
+
+    // Getters and Setters
+
+    public int getRequestId() {
+        return requestId;
+    }
+
+    public void setRequestId(int requestId) {
+        this.requestId = requestId;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public String getRequestStatus() {
+        return requestStatus;
+    }
+
+    public void setRequestStatus(String requestStatus) {
+        this.requestStatus = requestStatus;
+    }
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getAdminResponse() {
+        return adminResponse;
+    }
+
+    public void setAdminResponse(String adminResponse) {
+        this.adminResponse = adminResponse;
+    }
+
+    public String getRefundType() {
+        return refundType;
+    }
+
+    public void setRefundType(String refundType) {
+        this.refundType = refundType;
+    }
+
+    public double getRefundAmount() {
+        return refundAmount;
+    }
+
+    public void setRefundAmount(double refundAmount) {
+        this.refundAmount = refundAmount;
+    }
+
+    public LocalDateTime getRequestedAt() {
+        return requestedAt;
+    }
+
+    public void setRequestedAt(LocalDateTime requestedAt) {
+        this.requestedAt = requestedAt;
+    }
+
+    public LocalDateTime getProcessedAt() {
+        return processedAt;
+    }
+
+    public void setProcessedAt(LocalDateTime processedAt) {
+        this.processedAt = processedAt;
+    }
+}
